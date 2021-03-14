996e8cd4abc637874491569198c17744a1c8fb0f
Yan Abu Arab <tux1c@github.com>
2021-03-14 17:53:11 +0200

    auth cookies for private tweets

--- twitter_scraper/bt_scraper/rest.py
+++ twitter_scraper/bt_scraper/rest.py
@@ -1,13 +1,22 @@
 import requests
 
-def run_query(url, token = -1, cookie = -1):
+def run_query(url, token = -1, cookies = None):
     ua = "Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Goanna/4.8 Firefox/68.0 PaleMoon/29.0.1"
     auth = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+    c = None
+
 
     if (-1 == token):
         headers = { "User-Agent": ua }
     else:
-        headers = { "User-Agent": ua, "authorization": auth, "x-guest-token": token }
+        if (None == cookies):
+            headers = { "User-Agent": ua, "authorization": auth, "x-guest-token": token }
+        else:
+            headers = { "User-Agent": ua, "authorization": auth, "x-csrf-token": cookies['token'] }
+            c = { 'ct0': cookies['token'], 'auth_token': cookies['auth'] }
 
-    r = requests.get(url, headers=headers)
+    if (None == c):
+        r = requests.get(url, headers=headers)
+    else:
+        r = requests.get(url, headers=headers, cookies=c)
     return r.text
--- twitter_scraper/bt_scraper/tweet.py
+++ twitter_scraper/bt_scraper/tweet.py
@@ -13,3 +13,12 @@ class Tweet:
 
     def to_json(self):
         return json.dumps(self, default=lambda l: l.__dict__, sort_keys=True, indent=4)
+
+    def to_dict(self):
+        return {
+            'tweet_id': self.tweet_id,
+            'author_id': self.author_id,
+            'parent_id': self.parent_id,
+            'timestamp': self.timestamp,
+            'text': self.text
+        }
